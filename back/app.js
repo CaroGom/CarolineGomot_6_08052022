b29600a21d60fdd00101a56b47b708d25c62b1e4
carog <caroline.gomot@gmail.com>
2022-05-08 19:37:51 +0200

    setting up GET and POST routes in app.js

--- back/app.js
+++ back/app.js
@@ -1,13 +1,44 @@
 const express = require('express');
 
 const app = express();
+//setting up POST route
+app.use(express.json());
 
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
+
+//setting up GET route
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
-app.use((req, res, next) => {
-    res.json({message : 'Votre requête a bien été reçue !'})
+  });
+
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'fsjkshuijzb',
+            title: 'Mon premier Pingu',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://pbs.twimg.com/media/ED9ur_HWwAEcYk3.jpg',
+            price: 10,
+            userId: 'madPingu',
+        },
+        {
+            _id: 'dbuqjkiledjk',
+            title: 'Mon deuxième Pingu',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://img-08.stickers.cloud/packs/710aa9ed-5ddd-4f68-82d0-6bfafe6a4960/webp/2f392421-3544-4148-9178-065417a1801e.webp',
+            price: 11,
+            userId: 'hungryPingu',
+        }
+    ];
+    res.status(200).json(stuff);
 });
 
 module.exports = app;
